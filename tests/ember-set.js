558cea1d27c37aac67a614e6de581a0129ad3c66
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-11-08 02:06:40 -0800

    Update ember-set.js

--- tests/ember-set.js
+++ tests/ember-set.js
@@ -14,12 +14,32 @@ function code(program) {
 }
 
 describe('Ember.set transforms', function() {
+  it('skips AssignmentExpression where left is a VariableDeclarator (var)', () => {
+    assert.equal(code('var foo = true;'), `var foo = true;`);
+  });
+  
+  it('skips AssignmentExpression where left is a VariableDeclarator (let)', () => {
+    assert.equal(code('let foo = true;'), `let foo = true;`);
+  });
+  
+  it('skips AssignmentExpression where left is a VariableDeclarator (const)', () => {
+    assert.equal(code('const foo = true;'), `const foo = true;`);
+  });
+
+  it('skips AssignmentExpression where left is a VariableDeclarator (no declaration)', () => {
+    assert.equal(code('foo = true;'), `foo = true;`);
+  });
+ 
   it('converts assignmentExpression', () => {
     assert.equal(code('window.foo = true;'), `Ember.set(window, 'foo', true);`);
   });
 
   it('converts thisExpression', () => {
     assert.equal(code('this.foo = true;'), `Ember.set(this, 'foo', true);`);
+  });
+  
+  it('converts thisExpression', () => {
+    assert.equal(code('const SYM = Symbol(); this[SYM] = true;'), `Ember.set(this, ???, true);`);
   });
 
   it('converts memberExpressionLiterals', () => {
